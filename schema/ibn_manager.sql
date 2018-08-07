623bb3e056238bc7826d529ff805d0e6332dd0b5
ADEEL RAFIQ <rafiq.adeel@yahoo.com>
2018-08-07 18:19:52 +0900

    adding db schema

---
+++ schema/ibn_manager.sql
@@ -0,0 +1,60 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : ncl
+ Source Server Type    : MySQL
+ Source Server Version : 80011
+ Source Host           : localhost:3306
+ Source Schema         : ibn_manager
+
+ Target Server Type    : MySQL
+ Target Server Version : 80011
+ File Encoding         : 65001
+
+ Date: 07/08/2018 18:12:28
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for hibernate_sequence
+-- ----------------------------
+DROP TABLE IF EXISTS `hibernate_sequence`;
+CREATE TABLE `hibernate_sequence`  (
+  `next_val` bigint(20) NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for host_to_host_intent
+-- ----------------------------
+DROP TABLE IF EXISTS `host_to_host_intent`;
+CREATE TABLE `host_to_host_intent`  (
+  `hostToHostId` bigint(20) NOT NULL,
+  `version` bigint(20) NOT NULL,
+  `mac_address_src` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `mac_address_des` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `intent_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `priority` int(11) NULL DEFAULT NULL,
+  `application_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`hostToHostId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for point_to_point_intent
+-- ----------------------------
+DROP TABLE IF EXISTS `point_to_point_intent`;
+CREATE TABLE `point_to_point_intent`  (
+  `pointToPointId` bigint(20) NOT NULL,
+  `version` bigint(20) NOT NULL,
+  `device_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `intent_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `priority` int(11) NOT NULL,
+  `ingress_port` int(11) NOT NULL,
+  `egress_port` int(11) NOT NULL,
+  `application_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `mac_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`pointToPointId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
